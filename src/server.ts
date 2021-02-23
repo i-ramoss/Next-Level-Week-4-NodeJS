60c54413b205d8d64a29b706497579a3bc116333
Ian_Ramos <ianramossantos@hotmail.com>
2021-02-23 20:58:19 -0300

    Initial config of the database, typeorm and migrations

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
+import "reflect-metadata"
 import express from "express"
+import "./database";
 
 const app = express()
+
 
 app.get("/", (request, response) => {
   return response.json({ message: "Hello World - NLW #4" })
