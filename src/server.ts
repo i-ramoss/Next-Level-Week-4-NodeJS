e1fe18da78c638b8d46db6dc3f348673c51b345a
Ian_Ramos <ianramossantos@hotmail.com>
2021-02-23 00:13:33 -0300

    feat: Start nodejs project with typescript

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from "express"
+
+const app = express()
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello World - NLW #4" })
+})
+
+app.post("/", (request, response) => {
+  return response.json({ message: "The data are saved successfully!" })
+})
+
+app.listen(3333, () => console.log("Server is running"))
