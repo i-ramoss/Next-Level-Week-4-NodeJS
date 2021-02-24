b2b6789aeba64f54e64d8c495e79d9ceaf6949c2
Ian_Ramos <ianramossantos@hotmail.com>
2021-02-23 21:55:15 -0300

    Separate routes, create controllers and models
    - User typeorm migrations to manage the database more efficiently and practically

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,12 @@
 import "reflect-metadata"
 import express from "express"
+
 import "./database";
+import { router } from "./routes";
 
 const app = express()
 
-
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World - NLW #4" })
-})
-
-app.post("/", (request, response) => {
-  return response.json({ message: "The data are saved successfully!" })
-})
+app.use(express.json())
+app.use(router)
 
 app.listen(3333, () => console.log("Server is running"))
